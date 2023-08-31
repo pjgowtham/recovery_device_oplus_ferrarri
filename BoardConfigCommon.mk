5409c42584fea4254b0ab63d41999efed626de04
Captain Throwback <captainthrowback@hotmail.com>
2023-08-31 14:06:14 -0400

    sm84xx-common: simplify load modules list
    Only main modules need to be specified; others will be
    loaded as needed as dependencies
    
    Change-Id: Ic3093e55fe8a6bf81dea9c420475482909546a17

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,7 +163,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
-TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko rproc_qcom_common.ko q6_dlkm.ko qcom_q6v5.ko qcom_q6v5_pas.ko qcom_esoc.ko qcom_sysmon.ko tntfs.ko"
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko q6_dlkm.ko tntfs.ko"
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 
 # TWRP Debug Flags
